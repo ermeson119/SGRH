ab2f67c2b85e9bde768d29fcbe2e45635e8e5910
ermeson119 <ermesonbalbinot119@gmail.com>
2025-04-03 15:15:30 -0300

    feat: adicionado docker no sistemas e configurado banco

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Carrega o .env
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'chave_secreta'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sgrh.db'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
+    f"@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
